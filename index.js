22544ea3b4767e0442d7aa505ec73f65ae346328
ksmithut <github@ksmithut.com>
2022-04-18 08:34:40 -0600

    get auth redirect working

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+import crypto from 'node:crypto'
 import 'dotenv/config'
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import { fetch } from 'undici'
 
-const { PORT = '3000' } = process.env
+const {
+  PORT = '3000',
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  GITHUB_REDIRECT_URI,
+  COOKIE_SECRETS
+} = process.env
 
 const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', new URL('./views/', import.meta.url).pathname)
+
+app.use(cookieParser(COOKIE_SECRETS.trim().split(/\s+/)))
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' })
@@ -17,11 +28,41 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/login/github', (req, res) => {
-  res.status(501).send('Not Implemented')
+  const state = crypto.randomUUID()
+  const redirectURL = new URL('https://github.com/login/oauth/authorize')
+  redirectURL.searchParams.set('client_id', GITHUB_CLIENT_ID)
+  redirectURL.searchParams.set('redirect_uri', GITHUB_REDIRECT_URI)
+  redirectURL.searchParams.set('scope', 'user')
+  redirectURL.searchParams.set('state', state)
+  res.cookie('github_oauth_state', state, {
+    httpOnly: true,
+    maxAge: 300,
+    sameSite: 'strict',
+    signed: true
+  })
+  res.redirect(redirectURL.toString())
 })
 
-app.get('/login/github/callback', (req, res) => {
-  res.status(501).send('Not Implemented')
+app.get('/login/github/callback', (req, res, next) => {
+  const { code, state } = req.query
+  const reqState = req.signedCookies.github_oauth_state
+  if (state !== reqState) return res.redirect('/login')
+  const accessTokenURL = new URL('https://github.com/login/oauth/access_token')
+  accessTokenURL.searchParams.set('client_id', GITHUB_CLIENT_ID)
+  accessTokenURL.searchParams.set('client_secret', GITHUB_CLIENT_SECRET)
+  accessTokenURL.searchParams.set('code', code)
+  accessTokenURL.searchParams.set('redirect_uri', GITHUB_REDIRECT_URI)
+  fetch(accessTokenURL.toString(), {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      res.json(data)
+    })
+    .catch(next)
 })
 
 app.listen(PORT, () => {
